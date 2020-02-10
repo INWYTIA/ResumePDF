c08931f69441d84e41326a89a4bae948427f30c9
INWYTIA <inwytia@gmail.com>
2020-02-10 14:19:00 -0600

    corrected ansyncrinosity, writing to .html doc

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const convertFactory = require("electron-html-to");
 const generateHTML = require('./generateHTML.js');
 const axios = require('axios');
+// const conversion = convertFactory({
+//   converterPath: convertFactory.converters.PDF
+// });
 
 const userData = {
   name: '',
@@ -30,8 +33,51 @@ const questions = [
     }
 ];
 
+function buildData (url1) {
+  return new Promise(function(resolve, reject) {
+    if (url1 === `https://api.github.com/users/`) {
+      return reject(Error('Please enter a username.'));
+    };
+    axios.get(url1).then(function(result) {
+      userData.name = result.data.name;
+      userData.img = result.data.avatar_url;
+      userData.location = result.data.location;
+      userData.link = result.data.html_url;
+      userData.repos = result.data.public_repos;
+      userData.followers = result.data.followers;
+      userData.following = result.data.following;
+      const username = result.data.login;
+      return `https://api.github.com/users/${username}/repos?per_page=100`;
+    }).then(function (url2) {
+      return starData (url2);
+    }).then(function (stars) {
+      userData.stars = stars;
+      return resolve(userData);
+    });
+  });
+};
+
+function starData (url) {
+  return new Promise(function(resolve, reject) {
+    if (url === `https://api.github.com/users//repos?per_page=100`) {
+      return reject(Error('Please enter a username.'));
+    };
+    var userStars = 0;
+    axios.get(url).then(function(stars) {
+      stars.data.forEach(function (repos) {
+        userStars = userStars + repos.stargazers_count;
+      });
+      return resolve(userStars);
+    });
+  })
+};
+
 function writeToFile(fileName, data) {
- 
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      return console.log(err);
+    };
+  });
 };
 
 function init() {
@@ -41,32 +87,22 @@ function init() {
 init().then(function (ans) {
   userData.color = ans.color;
   const queryUserUrl = `https://api.github.com/users/${ans.username}`;
-  axios.get(queryUserUrl).then(function(result) {
-    userData.name = result.data.name;
-    userData.img = result.data.avatar_url;
-    userData.location = result.data.location;
-    userData.link = result.data.html_url;
-    userData.repos = result.data.public_repos;
-    userData.followers = result.data.followers;
-    userData.following = result.data.following;
-    const username = result.data.login
+  buildData(queryUserUrl).then(function (data) {
+    console.log(data);
+    const html = generateHTML.generateHTML(data);
+    writeToFile(`${ans.username}.html`,html);
+  }).catch(function(err) {
+    console.log(err);
   });
-  return `https://api.github.com/users/${username}/repos?per_page=100`;
-}).then(function (url) {
-  let userStars = 0;
-  axios.get(url).then(function(stars) {
-    stars.data.forEach(function (repos) {
-      userStars += repos.stargazers_count;
-    });
-  });
-  return userStars;
-}).then(function (starCount) {
-  userData.stars = starCount;
-  return userData;
-}).then(function (data) {
-  const html = generateHTML.generateHTML(data);
-  //fix the below two functions
-  writeToFile(,);
-  return readFile(,);
-})
-// then convert to pdf
+});
+  // fs.readFile(`${userData.login}.html`, function (err, data) {
+  //   if (err) {
+  //     return console.log(err);
+  //   };
+  //   conversion({ html: data }, (err, result) => {
+  //     if (err) {
+  //       return console.error(err);
+  //     };
+  //     result.stream.pipe(fs.createWriteStream(`${userData.login}.pdf`));
+  //   });
+  // });
